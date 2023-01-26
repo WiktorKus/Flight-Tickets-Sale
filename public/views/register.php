da5380373bb5ba1bfc4e23301c1333039bd0a7f8
WiktorKus <wiktorex.kus@gmail.com>
2023-01-26 11:13:10 +0100

    Add validation to password and login

--- public/views/register.php
+++ public/views/register.php
@@ -1,41 +1,42 @@
 <!DOCTYPE html>
 <head>
     <link rel="stylesheet" type="text/css" href="public/css/login.css">
+    <script type="text/javascript" src="./public/js/script.js" defer></script>
     <title>REGISTRATION PAGE</title>
 </head>
 <body>
-<div class="container">
-    <div class="logo">
-        AirJet
-        <img src="public/img/logo.svg">
-        <div class="logo-subtitle">
-            Your low cost airline
+    <div class="container">
+        <div class="logo">
+            AirJet
+            <img src="public/img/logo.svg">
+            <div class="logo-subtitle">
+                Your low cost airline
+            </div>
+        </div>
+        <div class="login-container">
+            <form class="login" action="register" method="POST" style="height: 90vh">
+                <div class="message">
+                    <?php if(isset($messages)) {
+                        foreach ($messages as $message) {
+                            echo $message;
+                        }
+                    }
+                    ?>
+                </div>
+                Login
+                <input name="email" type="text" placeholder="jan.kowalski@gmail.com" required>
+                Password
+                <input name="password" type="password" placeholder="**********" required>
+                Confirm password
+                <input name="confirmedPassword" type="password" placeholder="**********" required>
+                Name
+                <input name="name" type="text" placeholder="Jan">
+                Surname
+                <input name="surname" type="text" placeholder="Kowalski">
+                Phone
+                <input name="phone" type="text" placeholder="+48 555 222 111">
+                <button type="submit" class="btnLogIn">REGISTER</button>
+            </form>
         </div>
     </div>
-    <div class="login-container">
-        <form class="login" action="register" method="POST" style="height: 90vh">
-            <div class="message">
-                <?php if(isset($messages)) {
-                    foreach ($messages as $message) {
-                        echo $message;
-                    }
-                }
-                ?>
-            </div>
-            Login
-            <input name="email" type="text" placeholder="jan.kowalski@gmail.com">
-            Password
-            <input name="password" type="password" placeholder="**********">
-            Confirm password
-            <input name="confirmedPassword" type="password" placeholder="**********">
-            Name
-            <input name="name" type="text" placeholder="Jan">
-            Surname
-            <input name="surname" type="text" placeholder="Kowalski">
-            Phone
-            <input name="phone" type="text" placeholder="+48 555 222 111">
-            <button type="submit" class="btnLogIn">REGISTER</button>
-        </form>
-    </div>
-</div>
 </body>
